80fb3b58523712e7fa2c015cc5ba33a8f98b543d
NKumar-coder21 <62969623+NKumar-coder21@users.noreply.github.com>
2021-08-08 21:29:43 -0500

    Activity 4

---
+++ Activities/Activity 4/Activity 4 SecondsConvert/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿
+namespace Activity_4_SecondsConvert
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.numberOfSeconds = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbl_days = new System.Windows.Forms.Label();
+            this.lbl_hours = new System.Windows.Forms.Label();
+            this.lbl_minutes = new System.Windows.Forms.Label();
+            this.lbl_seconds = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfSeconds)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 80);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "# of seconds elapsed:";
+            // 
+            // numberOfSeconds
+            // 
+            this.numberOfSeconds.Location = new System.Drawing.Point(177, 78);
+            this.numberOfSeconds.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.numberOfSeconds.Name = "numberOfSeconds";
+            this.numberOfSeconds.Size = new System.Drawing.Size(120, 29);
+            this.numberOfSeconds.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(244, 42);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Convert elasped time (in seconds)\r\ninto days, hours and minutes";
+            // 
+            // lbl_days
+            // 
+            this.lbl_days.AutoSize = true;
+            this.lbl_days.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_days.Location = new System.Drawing.Point(13, 176);
+            this.lbl_days.Name = "lbl_days";
+            this.lbl_days.Size = new System.Drawing.Size(53, 55);
+            this.lbl_days.TabIndex = 3;
+            this.lbl_days.Text = "0";
+            // 
+            // lbl_hours
+            // 
+            this.lbl_hours.AutoSize = true;
+            this.lbl_hours.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_hours.Location = new System.Drawing.Point(13, 249);
+            this.lbl_hours.Name = "lbl_hours";
+            this.lbl_hours.Size = new System.Drawing.Size(53, 55);
+            this.lbl_hours.TabIndex = 4;
+            this.lbl_hours.Text = "0";
+            // 
+            // lbl_minutes
+            // 
+            this.lbl_minutes.AutoSize = true;
+            this.lbl_minutes.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_minutes.Location = new System.Drawing.Point(13, 322);
+            this.lbl_minutes.Name = "lbl_minutes";
+            this.lbl_minutes.Size = new System.Drawing.Size(53, 55);
+            this.lbl_minutes.TabIndex = 5;
+            this.lbl_minutes.Text = "0";
+            // 
+            // lbl_seconds
+            // 
+            this.lbl_seconds.AutoSize = true;
+            this.lbl_seconds.Font = new System.Drawing.Font("Arial Rounded MT Bold", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbl_seconds.Location = new System.Drawing.Point(13, 395);
+            this.lbl_seconds.Name = "lbl_seconds";
+            this.lbl_seconds.Size = new System.Drawing.Size(53, 55);
+            this.lbl_seconds.TabIndex = 6;
+            this.lbl_seconds.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(87, 407);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 30);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "seconds";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(87, 334);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 30);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "minute";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(87, 261);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 30);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "hours";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(87, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 30);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "days";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(35, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(244, 36);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(314, 471);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbl_seconds);
+            this.Controls.Add(this.lbl_minutes);
+            this.Controls.Add(this.lbl_hours);
+            this.Controls.Add(this.lbl_days);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numberOfSeconds);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Seconds Converter";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numberOfSeconds)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numberOfSeconds;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lbl_days;
+        private System.Windows.Forms.Label lbl_hours;
+        private System.Windows.Forms.Label lbl_minutes;
+        private System.Windows.Forms.Label lbl_seconds;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Activities/Activity 4/Activity 4 SecondsConvert/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_4_SecondsConvert
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int days = 0;
+            int hours = 0;
+            int minutes = 0;
+            int seconds = 0;
+
+            //checks if the entered seconds are days
+            if (numberOfSeconds.Value >= 86400)
+            {
+                days = (int)(numberOfSeconds.Value / 86400);
+                int remain1 = (int)(numberOfSeconds.Value % 86400);
+                //checks if remain1 can be used for hours
+                if(remain1 >= 3600)
+                {
+                    //calculates hours and remains
+                    hours = remain1 / 3600;
+                    int remain2 = remain1 % 3600;
+
+                    //checks remains2 for minutes and seconds
+                    if (remain2 >= 60)
+                    {
+                        minutes = remain2 / 60;
+                        seconds = remain2 % 60;
+                    }
+                    else
+                    {
+                        seconds = remain2;
+                    }
+                }
+                //checks if remain1 can be used for minutes
+                else if (remain1 >= 60)
+                {
+                    minutes = remain1 / 60;
+                    seconds = remain1 % 60;
+                }
+                //checks if the remain1 can be used for seconds
+                else
+                {
+                    seconds = remain1;
+                }
+            }
+            // checks if the entered seconds are hours
+            else if (numberOfSeconds.Value >= 3600)
+            {
+                    hours = (int)numberOfSeconds.Value / 3600;
+                    int remain1 = (int)numberOfSeconds.Value % 3600;
+
+                    if (remain1 >= 60)
+                    {
+                        minutes = remain1 / 60;
+                        seconds = remain1 % 60;
+                    }
+                    else
+                    {
+                        seconds = remain1;
+                    }
+                
+            }
+            //checks if the entered seconds are minutes
+            else if (numberOfSeconds.Value >= 60)
+            {
+                minutes = (int) numberOfSeconds.Value / 60;
+                seconds = (int) numberOfSeconds.Value % 60;
+            }
+            //checks if the entered seconds are seconds
+            else if (numberOfSeconds.Value < 60)
+            {
+                seconds = (int)numberOfSeconds.Value;
+            }
+
+            lbl_days.Text = days.ToString();
+            lbl_hours.Text = hours.ToString();
+            lbl_minutes.Text = minutes.ToString();
+            lbl_seconds.Text = seconds.ToString();
+        }
+    }
+}
