3f4e19bd51a49968b5f88e1184fea3e8796900c9
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-08-27 21:08:42 -0500

    Activity 9

---
+++ Activities/Activity 9/Activity9_Stubs/Activity9_Stubs/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Activity9_Stubs
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+        }
+        //1. Write a method that takes two ints and display their sum
+        public int addTwoNumbers(int num1, int num2)
+        { return 0; }
+
+        //2. Write a method that takes 5 doubles and return their average
+        public double doubleAverages(double num1, double num2, double num3, double num4, double num5)
+        { return 0; }
+
+        //3. Write a method that returns the sum of two randomly generated ints
+        public int addTwoRandomNumbers(int randomNum1, int randomNum2)
+        { return 0; }
+
+        //4. Write a method that takes three ints and returns true if sum divisable by 3, false if not
+        public bool divisableSumByThree(int num1, int num2, int num3)
+        { return false; }
+
+        //5. Write a method that takes two strings and display the string with fewer characters
+        public string findSmallerString(string string1, string string2)
+        { return null; }
+
+        //6. Write a method that takes an array of doubles and return largest value in array
+        public double largestDoubleInArray(double[] numberArray)
+        { return 0; }
+
+        //7. Write a method that generates and return array with 50 integers
+        public int[] generateIntArray()
+        { int[] myArray = null; return myArray; }
+
+        //8. Write a method that takes two bool variables and return true if match, false if not
+        public bool sameBoolValue(bool firstBool, bool secondBool)
+        { return false; }
+
+        //9. Write a method that takes an int and a double and multiply them to find product
+        public double addIntAndDouble(int number1, double number2)
+        { return 0; }
+
+        //10. Write a method that takes a 2-dimensional array of integers and returns the average of the array
+        public double twoDimensionalAverage(int[,] dimensionalArray)
+        { return 0; }
+    }
+}
