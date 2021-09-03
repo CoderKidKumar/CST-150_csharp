841edb26234716eb1b519f59d9c60e3a60670db6
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-09-03 17:02:54 -0500

    Activity 11

---
+++ Activities/Activity 11/Activity11_RollDice/Activity11_RollDice/Dice.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Activity11_RollDice
+{
+    internal class Dice
+    {
+        private int diceSides;
+        private int faceVal;
+        public Dice()
+        {
+            diceSides = 6;
+        }
+        public Dice(int sides)
+        {
+            if (sides >=4 && sides <= 20)
+            {
+                this.diceSides = sides;
+            }
+            else
+            {
+                Console.WriteLine("You cannot implement a dice that has " + sides + " sides! Making a dice with 6 sides...");
+                this.diceSides = 6;
+            }
+            
+        }
+
+        public int rollDice(Dice dice)
+        {
+            Random random = new Random();
+
+            dice.faceVal = random.Next(1, dice.diceSides + 1);
+
+            return dice.faceVal;
+        }
+    }
+}
---
+++ Activities/Activity 11/Activity11_RollDice/Activity11_RollDice/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Activity11_RollDice
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Dice dice1 = new Dice(12);
+            Dice dice2 = new Dice(12);
+
+            playGame(dice1, dice2);
+
+        }
+
+        private static void playGame(Dice dice1, Dice dice2)
+        {
+            int numRolls = 1;
+            bool isSnakeEyes = false;
+
+            while (isSnakeEyes == false)
+            {
+                int diceFace1, diceFace2;
+
+                diceFace1 = dice1.rollDice(dice1);
+                diceFace2 = dice2.rollDice(dice2);
+
+                Console.WriteLine("Roll #" + numRolls + ": " + diceFace1 + ", " + diceFace2);
+
+                if (diceFace1 == 1 && diceFace2 == 1)
+                {
+                    Console.WriteLine("It took " + numRolls + " rolls to get snake eyes! Phew!");
+                    isSnakeEyes = true;
+                }
+
+
+                numRolls++;
+            }
+        }
+    }
+}
