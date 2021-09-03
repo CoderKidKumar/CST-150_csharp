ec39557fcac3dc4fbe3783c2ee8d2833b9da498c
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-09-03 17:03:23 -0500

    Activity 12

---
+++ Activities/Activity 12/Activity12_FileEndWith/Activity12_FileEndWith/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+
+namespace Activity12_FileEndWith
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+           
+
+            string filepath = @"textDocument.txt";
+
+            Console.WriteLine("There are "+ findTE(textSplitter(readMyFile(filepath)))+" words that end with a 't' or an 'e' from the text above.");
+        }
+
+        private static String readMyFile(string filepath)
+        {
+            string filePlainText = File.ReadAllText(filepath);
+            Console.WriteLine(filePlainText + "\n");
+            return filePlainText;
+        }
+
+        //main method to find t and e
+        private static int findTE(string[] wordArray)
+        {
+            int countTE = 0;
+            for (int position = 0; position < wordArray.Length; position++)
+            {
+                string wordString = wordArray[position];
+                if (wordString.EndsWith("t") || wordString.EndsWith("e") || wordString.EndsWith("T") || wordString.EndsWith("E"))
+                    countTE++;
+            }
+            return countTE;
+        }
+
+        //method to split words in
+        private static String[] textSplitter(string fileText)
+        {
+            //set the chars to split the text by
+            char[] splittingChars = { ' ', ',', '.', ':', '\t' };
+
+            //use the split method, put each word into array, and remove empty entries
+            string[] words = fileText.Split(splittingChars, StringSplitOptions.RemoveEmptyEntries);
+
+            return words;
+        }
+    }
+}
