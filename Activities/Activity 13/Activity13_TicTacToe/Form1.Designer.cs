88c394f4e346c7c28d0d39624aae97f0a10d41e9
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-09-04 20:27:54 -0700

    Activity 13

---
+++ Activities/Activity 13/Activity13_TicTacToe/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿
+namespace Activity13_TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_1_1 = new System.Windows.Forms.Button();
+            this.btn_1_2 = new System.Windows.Forms.Button();
+            this.btn_1_0 = new System.Windows.Forms.Button();
+            this.btn_0_2 = new System.Windows.Forms.Button();
+            this.btn_0_1 = new System.Windows.Forms.Button();
+            this.btn_0_0 = new System.Windows.Forms.Button();
+            this.btn_2_0 = new System.Windows.Forms.Button();
+            this.btn_2_1 = new System.Windows.Forms.Button();
+            this.btn_2_2 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.itsTicTacToeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ticTacToeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.createdByNathanielToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_1_1
+            // 
+            this.btn_1_1.BackColor = System.Drawing.Color.White;
+            this.btn_1_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_1_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_1_1.FlatAppearance.BorderSize = 0;
+            this.btn_1_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1_1.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1_1.ForeColor = System.Drawing.Color.Black;
+            this.btn_1_1.Location = new System.Drawing.Point(295, 283);
+            this.btn_1_1.Name = "btn_1_1";
+            this.btn_1_1.Size = new System.Drawing.Size(228, 230);
+            this.btn_1_1.TabIndex = 0;
+            this.btn_1_1.UseVisualStyleBackColor = false;
+            this.btn_1_1.Click += new System.EventHandler(this.btn_1_1_Click);
+            // 
+            // btn_1_2
+            // 
+            this.btn_1_2.BackColor = System.Drawing.Color.White;
+            this.btn_1_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_1_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_1_2.FlatAppearance.BorderSize = 0;
+            this.btn_1_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1_2.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1_2.ForeColor = System.Drawing.Color.Black;
+            this.btn_1_2.Location = new System.Drawing.Point(538, 283);
+            this.btn_1_2.Name = "btn_1_2";
+            this.btn_1_2.Size = new System.Drawing.Size(228, 230);
+            this.btn_1_2.TabIndex = 1;
+            this.btn_1_2.UseVisualStyleBackColor = false;
+            this.btn_1_2.Click += new System.EventHandler(this.btn_1_2_Click);
+            // 
+            // btn_1_0
+            // 
+            this.btn_1_0.BackColor = System.Drawing.Color.White;
+            this.btn_1_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_1_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_1_0.FlatAppearance.BorderSize = 0;
+            this.btn_1_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_1_0.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_1_0.ForeColor = System.Drawing.Color.Black;
+            this.btn_1_0.Location = new System.Drawing.Point(51, 283);
+            this.btn_1_0.Name = "btn_1_0";
+            this.btn_1_0.Size = new System.Drawing.Size(228, 230);
+            this.btn_1_0.TabIndex = 2;
+            this.btn_1_0.UseVisualStyleBackColor = false;
+            this.btn_1_0.Click += new System.EventHandler(this.btn_1_0_Click);
+            // 
+            // btn_0_2
+            // 
+            this.btn_0_2.BackColor = System.Drawing.Color.White;
+            this.btn_0_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_0_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_0_2.FlatAppearance.BorderSize = 0;
+            this.btn_0_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0_2.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0_2.ForeColor = System.Drawing.Color.Black;
+            this.btn_0_2.Location = new System.Drawing.Point(538, 36);
+            this.btn_0_2.Name = "btn_0_2";
+            this.btn_0_2.Size = new System.Drawing.Size(228, 230);
+            this.btn_0_2.TabIndex = 3;
+            this.btn_0_2.UseVisualStyleBackColor = false;
+            this.btn_0_2.Click += new System.EventHandler(this.btn_0_2_Click);
+            // 
+            // btn_0_1
+            // 
+            this.btn_0_1.BackColor = System.Drawing.Color.White;
+            this.btn_0_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_0_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_0_1.FlatAppearance.BorderSize = 0;
+            this.btn_0_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0_1.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0_1.ForeColor = System.Drawing.Color.Black;
+            this.btn_0_1.Location = new System.Drawing.Point(295, 36);
+            this.btn_0_1.Name = "btn_0_1";
+            this.btn_0_1.Size = new System.Drawing.Size(228, 230);
+            this.btn_0_1.TabIndex = 4;
+            this.btn_0_1.UseVisualStyleBackColor = false;
+            this.btn_0_1.Click += new System.EventHandler(this.btn_0_1_Click);
+            // 
+            // btn_0_0
+            // 
+            this.btn_0_0.BackColor = System.Drawing.Color.White;
+            this.btn_0_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_0_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_0_0.FlatAppearance.BorderSize = 0;
+            this.btn_0_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_0_0.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_0_0.ForeColor = System.Drawing.Color.Black;
+            this.btn_0_0.Location = new System.Drawing.Point(51, 36);
+            this.btn_0_0.Name = "btn_0_0";
+            this.btn_0_0.Size = new System.Drawing.Size(228, 230);
+            this.btn_0_0.TabIndex = 5;
+            this.btn_0_0.UseVisualStyleBackColor = false;
+            this.btn_0_0.Click += new System.EventHandler(this.btn_0_0_Click);
+            // 
+            // btn_2_0
+            // 
+            this.btn_2_0.BackColor = System.Drawing.Color.White;
+            this.btn_2_0.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_2_0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_2_0.FlatAppearance.BorderSize = 0;
+            this.btn_2_0.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2_0.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2_0.ForeColor = System.Drawing.Color.Black;
+            this.btn_2_0.Location = new System.Drawing.Point(51, 529);
+            this.btn_2_0.Name = "btn_2_0";
+            this.btn_2_0.Size = new System.Drawing.Size(228, 230);
+            this.btn_2_0.TabIndex = 6;
+            this.btn_2_0.UseVisualStyleBackColor = false;
+            this.btn_2_0.Click += new System.EventHandler(this.btn_2_0_Click);
+            // 
+            // btn_2_1
+            // 
+            this.btn_2_1.BackColor = System.Drawing.Color.White;
+            this.btn_2_1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_2_1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_2_1.FlatAppearance.BorderSize = 0;
+            this.btn_2_1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2_1.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2_1.ForeColor = System.Drawing.Color.Black;
+            this.btn_2_1.Location = new System.Drawing.Point(295, 529);
+            this.btn_2_1.Name = "btn_2_1";
+            this.btn_2_1.Size = new System.Drawing.Size(228, 230);
+            this.btn_2_1.TabIndex = 7;
+            this.btn_2_1.UseVisualStyleBackColor = false;
+            this.btn_2_1.Click += new System.EventHandler(this.btn_2_1_Click);
+            // 
+            // btn_2_2
+            // 
+            this.btn_2_2.BackColor = System.Drawing.Color.White;
+            this.btn_2_2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn_2_2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_2_2.FlatAppearance.BorderSize = 0;
+            this.btn_2_2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_2_2.Font = new System.Drawing.Font("Lucida Sans Unicode", 110.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_2_2.ForeColor = System.Drawing.Color.Black;
+            this.btn_2_2.Location = new System.Drawing.Point(538, 529);
+            this.btn_2_2.Name = "btn_2_2";
+            this.btn_2_2.Size = new System.Drawing.Size(228, 230);
+            this.btn_2_2.TabIndex = 8;
+            this.btn_2_2.UseVisualStyleBackColor = false;
+            this.btn_2_2.Click += new System.EventHandler(this.btn_2_2_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(814, 24);
+            this.menuStrip1.TabIndex = 9;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newGameToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.gameToolStripMenuItem.Text = "Game";
+            // 
+            // newGameToolStripMenuItem
+            // 
+            this.newGameToolStripMenuItem.Name = "newGameToolStripMenuItem";
+            this.newGameToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newGameToolStripMenuItem.Text = "New Game";
+            this.newGameToolStripMenuItem.Click += new System.EventHandler(this.newGameToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ticTacToeToolStripMenuItem,
+            this.createdByNathanielToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.itsTicTacToeToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // itsTicTacToeToolStripMenuItem
+            // 
+            this.itsTicTacToeToolStripMenuItem.Name = "itsTicTacToeToolStripMenuItem";
+            this.itsTicTacToeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.itsTicTacToeToolStripMenuItem.Text = "It\'s TicTac Toe!";
+            // 
+            // ticTacToeToolStripMenuItem
+            // 
+            this.ticTacToeToolStripMenuItem.Name = "ticTacToeToolStripMenuItem";
+            this.ticTacToeToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.ticTacToeToolStripMenuItem.Text = "Tic Tac Toe";
+            // 
+            // createdByNathanielToolStripMenuItem
+            // 
+            this.createdByNathanielToolStripMenuItem.Name = "createdByNathanielToolStripMenuItem";
+            this.createdByNathanielToolStripMenuItem.Size = new System.Drawing.Size(185, 22);
+            this.createdByNathanielToolStripMenuItem.Text = "Created By Nathaniel";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::Activity13_TicTacToe.Properties.Resources.tictactoeboard2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(814, 791);
+            this.Controls.Add(this.btn_2_2);
+            this.Controls.Add(this.btn_2_1);
+            this.Controls.Add(this.btn_2_0);
+            this.Controls.Add(this.btn_0_0);
+            this.Controls.Add(this.btn_0_1);
+            this.Controls.Add(this.btn_0_2);
+            this.Controls.Add(this.btn_1_0);
+            this.Controls.Add(this.btn_1_2);
+            this.Controls.Add(this.btn_1_1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "TicTacToe";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_1_1;
+        private System.Windows.Forms.Button btn_1_2;
+        private System.Windows.Forms.Button btn_1_0;
+        private System.Windows.Forms.Button btn_0_2;
+        private System.Windows.Forms.Button btn_0_1;
+        private System.Windows.Forms.Button btn_0_0;
+        private System.Windows.Forms.Button btn_2_0;
+        private System.Windows.Forms.Button btn_2_1;
+        private System.Windows.Forms.Button btn_2_2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newGameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem itsTicTacToeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ticTacToeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem createdByNathanielToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ Activities/Activity 13/Activity13_TicTacToe/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity13_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        //set up variables to create the game (grid, position of the grid, and bool for player's turn and if the game is won or lost)
+        int[,] grid = { {0,0,0},
+                        {0,0,0},
+                        {0,0,0}};
+        int positionX;
+        int positionY;
+
+        bool isPlayerTurn = true;
+        bool isGameWon = false;
+        bool isGameLost = false;
+        //Random var for computer's turn
+        Random computerPosition = new Random();
+
+        //count for computer's tries - helps to prevent stack overflow
+        int computerTrys = 0;
+
+        // --------------------------------------------------------------------
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool checkPlayerTurn()
+        { // checks whos turn it is
+            if (isPlayerTurn == true)
+                return true;
+            else
+                return false;
+        }
+
+        private void changeGrid(int positionY, int positionX)
+        {
+            if (isPlayerTurn == true && isGameWon == false && isGameLost == false)
+            {
+                grid[positionY, positionX] = 1;
+
+                String buttonName = "btn_" + positionY + "_" + positionX;
+                this.Controls[buttonName].Text = "X";
+                this.Controls[buttonName].Enabled = false;
+                isPlayerTurn = false;
+                checkGameWon();
+                if (isGameWon == false) computerTurn();
+            }
+            else
+            {
+                String buttonName = "btn_" + positionY + "_" + positionX;
+                if (this.Controls[buttonName].Enabled == false && isGameWon == false && isGameLost == false)
+                {
+                    if(computerTrys != 100)
+                    {
+                        computerTrys++;
+                        computerTurn();
+                    }
+                    else
+                    {
+                        MessageBox.Show("So Close... It's a tie!", "Tie Game!", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+                    }
+                }
+                else
+                {
+                    grid[positionY, positionX] = 2;
+                    this.Controls[buttonName].Text = "O";
+                    this.Controls[buttonName].Enabled = false;
+
+                    checkGameWon();
+                }
+            }
+        }
+
+        private void checkGameWon()
+        {
+            if (grid[0, 0] == 1 && grid[0, 1] == 1 && grid[0, 2] == 1) isGameWon = true;
+            if (grid[1, 0] == 1 && grid[1, 1] == 1 && grid[1, 2] == 1) isGameWon = true;
+            if (grid[2, 0] == 1 && grid[2, 1] == 1 && grid[2, 2] == 1) isGameWon = true;
+
+            if (grid[0, 0] == 1 && grid[1, 0] == 1 && grid[2, 0] == 1) isGameWon = true;
+            if (grid[0, 1] == 1 && grid[1, 1] == 1 && grid[2, 1] == 1) isGameWon = true;
+            if (grid[0, 2] == 1 && grid[1, 2] == 1 && grid[2, 2] == 1) isGameWon = true;
+
+            if (grid[0, 0] == 1 && grid[1, 1] == 1 && grid[2, 2] == 1) isGameWon = true;
+            if (grid[0, 2] == 1 && grid[1, 1] == 1 && grid[2, 0] == 1) isGameWon = true;
+
+
+            if (grid[0, 0] == 2 && grid[0, 1] == 2 && grid[0, 2] == 2) isGameLost = true;
+            if (grid[1, 0] == 2 && grid[1, 1] == 2 && grid[1, 2] == 2) isGameLost = true;
+            if (grid[2, 0] == 2 && grid[2, 1] == 2 && grid[2, 2] == 2) isGameLost = true;
+
+            if (grid[0, 0] == 2 && grid[1, 0] == 2 && grid[2, 0] == 2) isGameLost = true;
+            if (grid[0, 1] == 2 && grid[1, 1] == 2 && grid[2, 1] == 2) isGameLost = true;
+            if (grid[0, 2] == 2 && grid[1, 2] == 2 && grid[2, 2] == 2) isGameLost = true;
+
+            if (grid[0, 0] == 2 && grid[1, 1] == 2 && grid[2, 2] == 2) isGameLost = true;
+            if (grid[0, 2] == 2 && grid[1, 1] == 2 && grid[2, 0] == 2) isGameLost = true;
+
+            if (isGameWon == true)
+            {
+                MessageBox.Show("You have won the game!", "You Won!", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+
+            if (isGameLost == true)
+            {
+                MessageBox.Show("Computer Won! \n You have lost the game!", "You Lost!", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+            }
+
+        }
+
+        private void computerTurn()
+        {
+            positionY = computerPosition.Next(3);
+            positionX = computerPosition.Next(3);
+
+            changeGrid(positionY, positionX);
+            isPlayerTurn = true;
+        }
+
+        private void btn_1_1_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 1;
+                positionX = 1;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_0_0_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 0;
+                positionX = 0;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_0_1_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 0;
+                positionX = 1;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_0_2_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 0;
+                positionX = 2;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_1_0_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 1;
+                positionX = 0;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_1_2_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 1;
+                positionX = 2;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_2_0_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 2;
+                positionX = 0;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_2_1_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 2;
+                positionX = 1;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void btn_2_2_Click(object sender, EventArgs e)
+        {
+            if (checkPlayerTurn() == true)
+            {
+                positionY = 2;
+                positionX = 2;
+
+                changeGrid(positionY, positionX);
+            }
+        }
+
+        private void newGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Application.Exit();
+        }
+    }
+}
