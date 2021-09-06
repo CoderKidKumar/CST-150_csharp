4480ad6b5186fb8fb1d5ff434907663983cfbe86
Nathaniel Kumar <62969623+NKumar-coder21@users.noreply.github.com>
2021-09-05 19:08:42 -0700

    Activity 14

---
+++ Activities/Activity 14/Activity14_FormControls/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿
+namespace Activity14_FormControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioBtn_DipWafCone = new System.Windows.Forms.RadioButton();
+            this.radioBtn_WafCone = new System.Windows.Forms.RadioButton();
+            this.radioBtn_SugCone = new System.Windows.Forms.RadioButton();
+            this.radioBtn_DipWafBowl = new System.Windows.Forms.RadioButton();
+            this.radioBtn_WafBowl = new System.Windows.Forms.RadioButton();
+            this.radioBtn_Bowl = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkBox_CarmMS = new System.Windows.Forms.CheckBox();
+            this.checkBox_CarmSyrup = new System.Windows.Forms.CheckBox();
+            this.checkBox_ChocMS = new System.Windows.Forms.CheckBox();
+            this.checkBox_StrawSyrup = new System.Windows.Forms.CheckBox();
+            this.checkBox_ChocSyrup = new System.Windows.Forms.CheckBox();
+            this.checkBox_Fudge = new System.Windows.Forms.CheckBox();
+            this.checkBox_Oreros = new System.Windows.Forms.CheckBox();
+            this.checkBox_Sprinkles = new System.Windows.Forms.CheckBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter The Order Details Here";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioBtn_DipWafCone);
+            this.groupBox1.Controls.Add(this.radioBtn_WafCone);
+            this.groupBox1.Controls.Add(this.radioBtn_SugCone);
+            this.groupBox1.Controls.Add(this.radioBtn_DipWafBowl);
+            this.groupBox1.Controls.Add(this.radioBtn_WafBowl);
+            this.groupBox1.Controls.Add(this.radioBtn_Bowl);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(17, 74);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(457, 88);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Serve Ice Cream In:";
+            // 
+            // radioBtn_DipWafCone
+            // 
+            this.radioBtn_DipWafCone.AutoSize = true;
+            this.radioBtn_DipWafCone.Location = new System.Drawing.Point(228, 52);
+            this.radioBtn_DipWafCone.Name = "radioBtn_DipWafCone";
+            this.radioBtn_DipWafCone.Size = new System.Drawing.Size(211, 20);
+            this.radioBtn_DipWafCone.TabIndex = 5;
+            this.radioBtn_DipWafCone.TabStop = true;
+            this.radioBtn_DipWafCone.Text = "Chocolate Dipped Waffle Cone";
+            this.radioBtn_DipWafCone.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_WafCone
+            // 
+            this.radioBtn_WafCone.AutoSize = true;
+            this.radioBtn_WafCone.Location = new System.Drawing.Point(121, 52);
+            this.radioBtn_WafCone.Name = "radioBtn_WafCone";
+            this.radioBtn_WafCone.Size = new System.Drawing.Size(99, 20);
+            this.radioBtn_WafCone.TabIndex = 4;
+            this.radioBtn_WafCone.TabStop = true;
+            this.radioBtn_WafCone.Text = "Waffle Cone";
+            this.radioBtn_WafCone.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_SugCone
+            // 
+            this.radioBtn_SugCone.AutoSize = true;
+            this.radioBtn_SugCone.Location = new System.Drawing.Point(14, 52);
+            this.radioBtn_SugCone.Name = "radioBtn_SugCone";
+            this.radioBtn_SugCone.Size = new System.Drawing.Size(97, 20);
+            this.radioBtn_SugCone.TabIndex = 3;
+            this.radioBtn_SugCone.TabStop = true;
+            this.radioBtn_SugCone.Text = "Sugar Cone";
+            this.radioBtn_SugCone.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_DipWafBowl
+            // 
+            this.radioBtn_DipWafBowl.AutoSize = true;
+            this.radioBtn_DipWafBowl.Location = new System.Drawing.Point(228, 26);
+            this.radioBtn_DipWafBowl.Name = "radioBtn_DipWafBowl";
+            this.radioBtn_DipWafBowl.Size = new System.Drawing.Size(200, 20);
+            this.radioBtn_DipWafBowl.TabIndex = 2;
+            this.radioBtn_DipWafBowl.TabStop = true;
+            this.radioBtn_DipWafBowl.Text = "Chocolate Diped Waffle Bowl";
+            this.radioBtn_DipWafBowl.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_WafBowl
+            // 
+            this.radioBtn_WafBowl.AutoSize = true;
+            this.radioBtn_WafBowl.Location = new System.Drawing.Point(121, 26);
+            this.radioBtn_WafBowl.Name = "radioBtn_WafBowl";
+            this.radioBtn_WafBowl.Size = new System.Drawing.Size(96, 20);
+            this.radioBtn_WafBowl.TabIndex = 1;
+            this.radioBtn_WafBowl.TabStop = true;
+            this.radioBtn_WafBowl.Text = "Waffle Bowl";
+            this.radioBtn_WafBowl.UseVisualStyleBackColor = true;
+            // 
+            // radioBtn_Bowl
+            // 
+            this.radioBtn_Bowl.AutoSize = true;
+            this.radioBtn_Bowl.Checked = true;
+            this.radioBtn_Bowl.Location = new System.Drawing.Point(14, 26);
+            this.radioBtn_Bowl.Name = "radioBtn_Bowl";
+            this.radioBtn_Bowl.Size = new System.Drawing.Size(55, 20);
+            this.radioBtn_Bowl.TabIndex = 0;
+            this.radioBtn_Bowl.TabStop = true;
+            this.radioBtn_Bowl.Text = "Bowl";
+            this.radioBtn_Bowl.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox_CarmMS);
+            this.groupBox2.Controls.Add(this.checkBox_CarmSyrup);
+            this.groupBox2.Controls.Add(this.checkBox_ChocMS);
+            this.groupBox2.Controls.Add(this.checkBox_StrawSyrup);
+            this.groupBox2.Controls.Add(this.checkBox_ChocSyrup);
+            this.groupBox2.Controls.Add(this.checkBox_Fudge);
+            this.groupBox2.Controls.Add(this.checkBox_Oreros);
+            this.groupBox2.Controls.Add(this.checkBox_Sprinkles);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(257, 174);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(188, 231);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Toppings";
+            // 
+            // checkBox_CarmMS
+            // 
+            this.checkBox_CarmMS.AutoSize = true;
+            this.checkBox_CarmMS.Location = new System.Drawing.Point(6, 175);
+            this.checkBox_CarmMS.Name = "checkBox_CarmMS";
+            this.checkBox_CarmMS.Size = new System.Drawing.Size(161, 20);
+            this.checkBox_CarmMS.TabIndex = 7;
+            this.checkBox_CarmMS.Text = "Caramel \"Magic Shell\"";
+            this.checkBox_CarmMS.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_CarmSyrup
+            // 
+            this.checkBox_CarmSyrup.AutoSize = true;
+            this.checkBox_CarmSyrup.Location = new System.Drawing.Point(6, 149);
+            this.checkBox_CarmSyrup.Name = "checkBox_CarmSyrup";
+            this.checkBox_CarmSyrup.Size = new System.Drawing.Size(116, 20);
+            this.checkBox_CarmSyrup.TabIndex = 6;
+            this.checkBox_CarmSyrup.Text = "Caramel Syrup";
+            this.checkBox_CarmSyrup.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_ChocMS
+            // 
+            this.checkBox_ChocMS.AutoSize = true;
+            this.checkBox_ChocMS.Location = new System.Drawing.Point(6, 201);
+            this.checkBox_ChocMS.Name = "checkBox_ChocMS";
+            this.checkBox_ChocMS.Size = new System.Drawing.Size(171, 20);
+            this.checkBox_ChocMS.TabIndex = 5;
+            this.checkBox_ChocMS.Text = "Chocolate \"Magic Shell\"";
+            this.checkBox_ChocMS.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_StrawSyrup
+            // 
+            this.checkBox_StrawSyrup.AutoSize = true;
+            this.checkBox_StrawSyrup.Location = new System.Drawing.Point(6, 123);
+            this.checkBox_StrawSyrup.Name = "checkBox_StrawSyrup";
+            this.checkBox_StrawSyrup.Size = new System.Drawing.Size(129, 20);
+            this.checkBox_StrawSyrup.TabIndex = 4;
+            this.checkBox_StrawSyrup.Text = "Strawberry Syrup";
+            this.checkBox_StrawSyrup.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_ChocSyrup
+            // 
+            this.checkBox_ChocSyrup.AutoSize = true;
+            this.checkBox_ChocSyrup.Location = new System.Drawing.Point(6, 97);
+            this.checkBox_ChocSyrup.Name = "checkBox_ChocSyrup";
+            this.checkBox_ChocSyrup.Size = new System.Drawing.Size(126, 20);
+            this.checkBox_ChocSyrup.TabIndex = 3;
+            this.checkBox_ChocSyrup.Text = "Chocolate Syrup";
+            this.checkBox_ChocSyrup.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_Fudge
+            // 
+            this.checkBox_Fudge.AutoSize = true;
+            this.checkBox_Fudge.Location = new System.Drawing.Point(6, 71);
+            this.checkBox_Fudge.Name = "checkBox_Fudge";
+            this.checkBox_Fudge.Size = new System.Drawing.Size(90, 20);
+            this.checkBox_Fudge.TabIndex = 2;
+            this.checkBox_Fudge.Text = "Hot Fudge";
+            this.checkBox_Fudge.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_Oreros
+            // 
+            this.checkBox_Oreros.AutoSize = true;
+            this.checkBox_Oreros.Location = new System.Drawing.Point(6, 45);
+            this.checkBox_Oreros.Name = "checkBox_Oreros";
+            this.checkBox_Oreros.Size = new System.Drawing.Size(117, 20);
+            this.checkBox_Oreros.TabIndex = 1;
+            this.checkBox_Oreros.Text = "Crushed Oreos";
+            this.checkBox_Oreros.UseVisualStyleBackColor = true;
+            // 
+            // checkBox_Sprinkles
+            // 
+            this.checkBox_Sprinkles.AutoSize = true;
+            this.checkBox_Sprinkles.Location = new System.Drawing.Point(6, 19);
+            this.checkBox_Sprinkles.Name = "checkBox_Sprinkles";
+            this.checkBox_Sprinkles.Size = new System.Drawing.Size(83, 20);
+            this.checkBox_Sprinkles.TabIndex = 0;
+            this.checkBox_Sprinkles.Text = "Sprinkles";
+            this.checkBox_Sprinkles.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "Chocolate",
+            "Vanilla",
+            "Strawberry",
+            "Mint Chocolate Chip",
+            "Butter Pecan",
+            "Cookies N\' Cream",
+            "Rocky Road",
+            "Chocolate Chip",
+            "Birthday Cake",
+            "Banana Split",
+            "Mint",
+            "Cookie Dough",
+            "Cotton Candy"});
+            this.listBox1.Location = new System.Drawing.Point(17, 193);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(204, 212);
+            this.listBox1.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(28, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Flavors:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(17, 432);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(457, 35);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Add Order";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(488, 479);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioBtn_DipWafCone;
+        private System.Windows.Forms.RadioButton radioBtn_WafCone;
+        private System.Windows.Forms.RadioButton radioBtn_SugCone;
+        private System.Windows.Forms.RadioButton radioBtn_DipWafBowl;
+        private System.Windows.Forms.RadioButton radioBtn_WafBowl;
+        private System.Windows.Forms.RadioButton radioBtn_Bowl;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBox_CarmMS;
+        private System.Windows.Forms.CheckBox checkBox_CarmSyrup;
+        private System.Windows.Forms.CheckBox checkBox_ChocMS;
+        private System.Windows.Forms.CheckBox checkBox_StrawSyrup;
+        private System.Windows.Forms.CheckBox checkBox_ChocSyrup;
+        private System.Windows.Forms.CheckBox checkBox_Fudge;
+        private System.Windows.Forms.CheckBox checkBox_Oreros;
+        private System.Windows.Forms.CheckBox checkBox_Sprinkles;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Activities/Activity 14/Activity14_FormControls/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity14_FormControls
+{
+    public partial class Form1 : Form
+    {
+        string chosenFlavors, chosenToppings, creation_Message;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            createOrder();
+            resetMessage();
+        }
+
+        private void resetMessage()
+        {
+            creation_Message = "";
+            chosenToppings = "";
+            chosenFlavors = "";
+        }
+
+        private void createOrder()
+        {
+            //get the radio button choice
+            string iceCreamBase = groupBox1.Controls.OfType<RadioButton>().FirstOrDefault(btn => btn.Checked).Text;
+
+            //set up a string that holds all the chosen flavors
+            foreach (String flavor in listBox1.SelectedItems)
+                chosenFlavors += flavor + "\n";
+
+            // get all the topping that were selected
+            if (checkBox_Sprinkles.Checked)
+                chosenToppings += "sprinkles\n";
+            if (checkBox_Oreros.Checked)
+                chosenToppings += "crushed oreos\n";
+            if (checkBox_Fudge.Checked)
+                chosenToppings += "hot fudge\n";
+            if (checkBox_ChocSyrup.Checked)
+                chosenToppings += "chocolate syrup\n";
+            if (checkBox_StrawSyrup.Checked)
+                chosenToppings += "strawberry syrup\n";
+            if (checkBox_CarmSyrup.Checked)
+                chosenToppings += "caramel syrup\n";
+            if (checkBox_CarmMS.Checked)
+                chosenToppings += "caramel magic shell\n";
+            if (checkBox_ChocMS.Checked)
+                chosenToppings += "chocolate magic shell\n";
+
+
+            creation_Message = "You made an order for: \n"+chosenFlavors+"\nTopped with:\n"+chosenToppings+"\nAll served in a " + iceCreamBase;
+            MessageBox.Show(creation_Message);
+        }
+    }
+}
